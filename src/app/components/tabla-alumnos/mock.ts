6473ba8db507e49bef974a26d3de340c9f6f0697
davefn98 <118871110+davefn98@users.noreply.github.com>
2024-01-08 12:36:22 -0500

    Lista Cursos Angular

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] = [
+    {
+        nombre: 'Ricardo',
+        apellido: 'Perez',
+        promedio: 8.5
+    },
+    {
+        nombre: 'Florencia',
+        apellido: 'Perez',
+        promedio: 9
+    },
+    {
+        nombre: 'Pedro',
+        apellido: 'Perez',
+        promedio: 6
+    }
+]
